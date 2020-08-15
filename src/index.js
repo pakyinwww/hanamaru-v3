05077c632e159cb6daed9ad9cf45dd01ed1a0d47
pakyinwww <pakyinwww@gmail.com>
2020-08-15 18:20:59 +0800

    build(core): Linter added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-const root = document.getElementById('root')
+import React from "react"
+import ReactDom from "react-dom"
+
+const root = document.getElementById("root")
 const App = () => {
-    return (
-        <h1>Hello from React</h1>
-    )
+  return <h1>Hello from React</h1>
 }
+
 ReactDom.render(<App />, root)
